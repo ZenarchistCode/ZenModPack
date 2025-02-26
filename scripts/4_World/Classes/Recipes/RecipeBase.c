ad8a6e1646ecfa39044d1f8b6fe0fb64aa8c89b5
Pechyvo <48208976+Pechyvo@users.noreply.github.com>
2025-02-26 19:39:38 +0100

    DayZ 1.27 Compile Fix

--- scripts/4_World/Classes/Recipes/RecipeBase.c
+++ scripts/4_World/Classes/Recipes/RecipeBase.c
@@ -4,27 +4,27 @@ modded class RecipeBase
 
 	// Hi-jack ingredient sound here whenever appropriate to cover multiple recipes
 	// and to allow compatability with future vanilla updates (InsertIngredientEx() will be used directly to add sounds and this would be ignored)
-	override void InsertIngredient(int index, string ingredient)
+	override void InsertIngredient(int index, string ingredient, DayZPlayerConstants uid = DayZPlayerConstants.CMD_ACTIONFB_CRAFTING)
 	{
 		if (!ZenModEnabled("ZenCraftingSounds"))
 		{
-			super.InsertIngredient(index, ingredient);
+			super.InsertIngredient(index, ingredient, uid);
 			return;
 		}
 
 		switch (ingredient)
 		{
 			case "SewingKit":
-				InsertIngredientEx(index, ingredient, "Zen_SewingKit");
+				InsertIngredientEx(index, ingredient, "Zen_SewingKit", uid);
 				return;
 			case "LeatherSewingKit":
-				InsertIngredientEx(index, ingredient, "Zen_LeatherSewingKit");
+				InsertIngredientEx(index, ingredient, "Zen_LeatherSewingKit", uid);
 				return;
 			case "Whetstone":
-				InsertIngredientEx(index, ingredient, "Zen_SharpenMelee");
+				InsertIngredientEx(index, ingredient, "Zen_SharpenMelee", uid);
 				return;
 			case "WeaponCleaningKit":
-				InsertIngredientEx(index, ingredient, "Zen_CleanWeapon");
+				InsertIngredientEx(index, ingredient, "Zen_CleanWeapon", uid);
 				return;
 			case "DisinfectantAlcohol":
 			case "IodineTincture":
@@ -33,50 +33,50 @@ modded class RecipeBase
 			case "SyringeFull":
 			case "AmpouleAlcohol":
 			case "SyberiaMedicineAmpoule":
-				InsertIngredientEx(index, ingredient, "Zen_DisinfectBottle");
+				InsertIngredientEx(index, ingredient, "Zen_DisinfectBottle", uid);
 				return;
 			case "DisinfectantSpray":
-				InsertIngredientEx(index, ingredient, "Zen_DisinfectSpray");
+				InsertIngredientEx(index, ingredient, "Zen_DisinfectSpray", uid);
 				return;
 			case "DuctTape":
-				InsertIngredientEx(index, ingredient, "Zen_DuctTape");
+				InsertIngredientEx(index, ingredient, "Zen_DuctTape", uid);
 				return;
 			case "Spraycan_ColorBase":
 			case "Zen_Dye_Black":
 			case "Zen_Dye_Green":
-				InsertIngredientEx(index, ingredient, "Zen_SprayPaint");
+				InsertIngredientEx(index, ingredient, "Zen_SprayPaint", uid);
 				return;
 			case "TireRepairKit":
-				InsertIngredientEx(index, ingredient, "Zen_Rubber");
+				InsertIngredientEx(index, ingredient, "Zen_Rubber", uid);
 				return;
 			case "SmallStone":
 			case "Stone":
-				InsertIngredientEx(index, ingredient, "Zen_BreakStone");
+				InsertIngredientEx(index, ingredient, "Zen_BreakStone", uid);
 				return;
 			case "BarbedWire":
-				InsertIngredientEx(index, ingredient, "Zen_MetalWire");
+				InsertIngredientEx(index, ingredient, "Zen_MetalWire", uid);
 				return;
 			case "WoodenPlank":
-				InsertIngredientEx(index, ingredient, "Zen_Wood");
+				InsertIngredientEx(index, ingredient, "Zen_Wood", uid);
 				return;
 			case "Nail":
-				InsertIngredientEx(index, ingredient, "Zen_Nails");
+				InsertIngredientEx(index, ingredient, "Zen_Nails", uid);
 				return;
 			case "Flag_Base":
 			case "Armband_ColorBase":
 			case "Raincoat_ColorBase":
-				InsertIngredientEx(index, ingredient, "Rags_craft");
+				InsertIngredientEx(index, ingredient, "Rags_craft", uid);
 				return;
 			case "ElectronicRepairKit":
-				InsertIngredientEx(index, ingredient, "Zen_ElectronicRepair");
+				InsertIngredientEx(index, ingredient, "Zen_ElectronicRepair", uid);
 				return;
 			case "Paper":
-				InsertIngredientEx(index, ingredient, "Zen_Paper");
+				InsertIngredientEx(index, ingredient, "Zen_Paper", uid);
 				return;
 			default:
 				break;
 		}
 
-		InsertIngredientEx(index, ingredient, "");
+		InsertIngredientEx(index, ingredient, "", uid);
 	};
 }
